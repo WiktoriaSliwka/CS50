9dbeccedeaee5fe9776e014b31d3a8ead75cd313
WSliwka <96419794+WiktoriaSliwka@users.noreply.github.com>
2025-04-14 15:48:37 +0100

    week0

---
+++ week0/functions/functions.py
@@ -0,0 +1,7 @@
+#print("Hello world!")  #built in function
+# an argument is an input to a function that somehow influences its behavior 
+# () parentheses
+# a bug is a mistake 
+name = input("what's your name g?")
+print("hey")
+print(name)
